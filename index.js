d2b319af0ab2c3f9594b054241d9668df569c8b8
Hawa <hawa.mjh@gmail.com>
2023-04-15 15:35:51 +0000

    lightcoin

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    } else {
+      return false;
+    }
   }
-
 }
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
 
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+  isAllowed() {
+    return true;
+  }
+}
 
-t1 = new Withdrawal(50.25);
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+const myAccount = new Account('snow-patrol');
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
+const t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 1:', t1);
 console.log('Transaction 2:', t2);
+console.log('Transaction 3:', t3);
+console.log('Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+const t4 = new Withdrawal(50.25, myAccount);
+t4.commit();
+console.log('Transaction 4:', t4);
+
+const t5 = new Withdrawal(9.99, myAccount);
+t5.commit();
+console.log('Transaction 5:', t5);
+
+console.log('Balance:', myAccount.balance);
